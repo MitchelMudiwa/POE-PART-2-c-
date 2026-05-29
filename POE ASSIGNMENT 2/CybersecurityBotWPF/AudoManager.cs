a3f0d210828beaca2998ede40ad2a7b9032ec984
mudiwa.dev <ST10496035@rcconnect.edu.za>
2026-05-29 21:43:58 +0200

    Add files via upload

---
+++ POE ASSIGNMENT 2/CybersecurityBotWPF/AudoManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace CybersecurityBotWPF
+{
+    public static class AudioManager
+    {
+        public static void PlayGreeting()
+        {
+            try
+            {
+                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    return;
+
+                string audioPath = Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory, "greeting.wav");
+
+                if (File.Exists(audioPath))
+                {
+                    using var player = new System.Media.SoundPlayer(audioPath);
+                    player.Play();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"AudioManager.PlayGreeting error: {ex}");
+            }
+        }
+    }
+}
